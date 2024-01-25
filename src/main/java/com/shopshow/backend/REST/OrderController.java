8adcb47338ef9a6f5c5d8f391d1621a8b24a2579
Ritesh18117 <rgriteshbyahut18117rg@gmail.com>
2024-01-25 16:58:31 +0530

    Making "placeOrder" API

--- src/main/java/com/shopshow/backend/REST/OrderController.java
+++ src/main/java/com/shopshow/backend/REST/OrderController.java
@@ -1,10 +1,10 @@
 package com.shopshow.backend.REST;
 
+import com.shopshow.backend.dto.OrderRequest;
 import com.shopshow.backend.entities.Order;
 import com.shopshow.backend.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,14 +19,15 @@ public class OrderController {
     public String test(){
         return "This is test from Order Controller!";
     }
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/getAll")
     public ResponseEntity<List<Order>> getAllOrder(){
         return orderService.getAllOrder();
     }
 //    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
-    @PostMapping("/addOrder")
-    public ResponseEntity<Order> addOrder(@RequestBody Order order){
-        return orderService.addOrder(order);
+    @PostMapping("/placeOrder")
+    // https://chat.openai.com/share/665ce237-6222-4842-82ec-73e8254153e4
+    public ResponseEntity<String> addOrder(@RequestHeader(value = "Authorization") String authorizationHeader, @RequestBody OrderRequest orderRequest){
+        return orderService.placeOrder(authorizationHeader,orderRequest);
     }
 }
--- src/main/java/com/shopshow/backend/services/OrderService.java
+++ src/main/java/com/shopshow/backend/services/OrderService.java
@@ -1,13 +1,23 @@
 package com.shopshow.backend.services;
 
+import com.shopshow.backend.dao.CustomerRepository;
 import com.shopshow.backend.dao.OrderRepository;
+import com.shopshow.backend.dao.ProductRepository;
+import com.shopshow.backend.dao.UserRepository;
+import com.shopshow.backend.dto.OrderRequest;
+import com.shopshow.backend.dto.ProductRequest;
+import com.shopshow.backend.entities.Address;
+import com.shopshow.backend.entities.Customer;
 import com.shopshow.backend.entities.Order;
+import com.shopshow.backend.entities.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,6 +25,14 @@ import java.util.Optional;
 public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
+    @Autowired
+    private JwtService jwtService;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
+    private ProductRepository productRepository;
 
     public ResponseEntity<List<Order>> getAllOrder(){
         try{
@@ -29,14 +47,42 @@ public class OrderService {
         }
     }
 
-    public ResponseEntity<Order> addOrder(@RequestBody Order order){
+    public ResponseEntity<String> placeOrder(@RequestHeader(value = "Authorization") String authorizationHeader, OrderRequest orderRequest){
         try{
-            orderRepository.save(order);
-            return ResponseEntity.of(Optional.of(order));
+            String token = extractTokenFromHeader(authorizationHeader);
+            String username = jwtService.extractUsername(token);
+            Long userId = userRepository.findByUsername(username).getId();
+            Customer customer = customerRepository.findByUserId(userId);
+            List<ProductRequest> products = orderRequest.getProducts();
+            Address address = orderRequest.getAddress();
+            for(ProductRequest product : products){
+                Order order = new Order();
+                order.setCustomer(customer);
+                Optional<Product> productOptional = productRepository.findById(product.getId());
+                productOptional.ifPresent(product1 -> order.setProduct(product1));
+                order.setAddress(orderRequest.getAddress());
+                order.setOrderStatus("false");
+                order.setOrderDate(Date.valueOf(LocalDate.now()));
+                order.setQuantity(product.getQuantity());
+                order.setComment(orderRequest.getComment());
+                order.setDateOfDelivery(Date.valueOf(LocalDate.now().plusDays(7)));
+                productOptional.ifPresent(product1 -> order.setTotalPrice((product1.getPrice() * product.getQuantity()) - ((product.getQuantity() * product1.getPrice())/100 * product1.getDiscount())));
+                orderRepository.save(order);
+            }
+            return ResponseEntity.ok("Order Placed Successfully!");
         } catch (Exception e){
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
+    }
+
+    private String extractTokenFromHeader(String authorizationHeader) {
+        // Check if the Authorization header is not null and starts with "Bearer "
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            // Extract the token part by removing "Bearer " prefix
+            return authorizationHeader.substring(7); // "Bearer ".length() == 7
+        }
+        return null; // Return null or handle accordingly if token extraction fails
     }
 
 }
