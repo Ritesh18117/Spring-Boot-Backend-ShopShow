e0aeb9297c5938b09a2715c84b1e1a1360af2a82
Ritesh18117 <rgriteshbyahut18117rg@gmail.com>
2024-02-13 22:25:02 +0530

    Changin Database Removing Color and Size Table

--- src/main/java/com/shopshow/backend/dao/ColorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.shopshow.backend.dao;
-
-import com.shopshow.backend.entities.Color;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ColorRepository extends CrudRepository<Color,Long> {
-    Color findByColor(String color);
-}
--- src/main/java/com/shopshow/backend/dao/SizeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.shopshow.backend.dao;
-
-import com.shopshow.backend.entities.Size;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SizeRepository extends CrudRepository<Size,Long> {
-    Size findBySize(String size);
-}
